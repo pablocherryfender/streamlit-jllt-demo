d16e381fd827e5f03ce1d81d1fce035a3f82a7ce
Pablo <114480204+pablocherryfender@users.noreply.github.com>
2024-10-20 20:13:19 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+# Since sudo is not available, you can download and install pip manually by following these steps:
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py --user
+
+# Verify pip install
+~/.local/bin/pip --version
+
+# Install Streamlit 
+~/.local/bin/pip install streamlit --user
+
+
